c5778461fdd4b89a818472fddd99a94661f11d7a
ludivicoB <ludivicobalaterojr@gmail.com>
2026-01-05 15:20:55 +0800

    Added auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,23 +1,40 @@
 import { Request, Response, NextFunction } from "express";
-import { supabase } from "../config/supabase.client";
+import { JwtUtils } from "../utils/jwt";
+
+export interface AuthRequest extends Request {
+  user?: {
+    id: string;
+  };
+}
 
 export const authenticate = async (
-  req: Request,
+  req: AuthRequest,
   res: Response,
   next: NextFunction
 ) => {
-  const authHeader = req.headers.authorization;
+  try {
+    const authHeader = req.headers.authorization;
 
-  if (!authHeader)
-    return res.status(401).json({ message: "No token provided" });
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        success: false,
+        message: "No token provided",
+      });
+    }
 
-  const token = authHeader.split(" ")[1];
+    const token = authHeader.split(" ")[1];
 
-  const { data: user, error } = await supabase.auth.getUser(token);
+    const decoded = JwtUtils.verifyToken(token as string);
 
-  if (error || !user) return res.status(401).json({ message: "Invalid token" });
+    req.user = {
+      id: decoded.user_id,
+    };
 
-  // attach user to request
-  (req as any).user = user.user;
-  next();
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
+    });
+  }
 };
